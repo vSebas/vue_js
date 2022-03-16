3bd9e90088c9f6df51be2b2200e3dfa6d5bf41c3
Sebastian Martínez <55563384+vSebas@users.noreply.github.com>
2022-03-15 20:44:37 -0600

    Vue js activity

---
+++ basic.js
@@ -0,0 +1,17 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Buenas buenas!'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        bucl:[
+            { message:"Ah, caray"  },
+            { message:"Otto" },
+            { message:'Harry' },
+        ]
+    }
+  })
